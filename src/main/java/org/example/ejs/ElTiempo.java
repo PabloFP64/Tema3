a296c2011822012943493cfb1168f0844f02b000
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-02-26 12:40:00 +0100

    1º hecho

---
+++ src/main/java/org/example/ejs/ElTiempo.java
@@ -0,0 +1,18 @@
+package org.example.ejs;
+
+import java.util.Scanner;
+
+public class ElTiempo{
+    //Ejercicio1
+
+    public void riesgoHielo(Scanner teclado){
+        System.out.println("Introduce la temperatura en grados centígrados(ºC): ");
+        double temperatura = teclado.nextFloat();
+
+        if(temperatura < 2){
+            System.out.println("!La temperatura es inferior a 2º¡ !Riesgo de hielo!");
+        }else{
+            System.out.println("No hay riesgo de hielo");
+        }
+    }
+}
